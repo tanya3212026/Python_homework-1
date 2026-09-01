830cb843588946d3919ae15c258d28b7500f6257
o.guman <o.guman@mob-edu.ru>
2026-09-01 17:32:47 +0700

    first homework Python

---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,4 @@
+# Получить пользовательский ввод
+first_name = input('Ваше имя')
+last_name = input('Ваша фамилия')
+print('Вас зовут: ' + last_name + first_name)
---
+++ lesson_01/lesson_1_task_4.py
@@ -0,0 +1,4 @@
+# Создать функцию
+def print_greeting():
+    print('Привет, мир!')
+    print_greeting()
